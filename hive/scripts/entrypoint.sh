e7e1906059d510abf4975e250a7d54bb835f1c49
Vinay Malkani <vinay.malkani@f8-federal.com>
2021-05-07 00:13:59 -0400

    add supervisord

--- hive/scripts/entrypoint.sh
+++ hive/scripts/entrypoint.sh
@@ -3,6 +3,6 @@
 [[ -n "${DB_URI}" ]] && wait-for ${DB_URI}
 
 [[ ! -f initSchema.completed ]] && schematool -dbType mysql -initSchema && touch initSchema.completed
-hive --service metastore
+#hive --service metastore
+supervisord -c /etc/supervisord.conf -n
 
-/opt/apache-hive-3.1.2-bin/bin/hiveserver2
